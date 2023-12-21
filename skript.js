aacd9df007a8fd484febcfea7b5c7f0e52aaf98b
Sofija Milic <sofijamilic2005@gmail.com>
2023-12-21 03:44:40 +0100

    sredjen skript

--- skript.js
+++ skript.js
@@ -2,13 +2,6 @@ document.getElementById("mojeDugme").addEventListener("click", function() {
     window.location.href = "smbio.html";
   });
 
-  document.getElementById("vratiNazad").addEventListener("click", function() {
-    window.location.href = "smportfolio.html";
-  });
 
-
-  /*document.getElementById("vratiNazad").addEventListener("click", function() {
-    window.history.back();
-  });*/
   
   
